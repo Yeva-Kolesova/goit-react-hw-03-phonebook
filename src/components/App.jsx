1e1e6d4a43b10d73ef0d4fc1533598cd20edec68
Kolesova Yeva <137623500+Yeva-Kolesova@users.noreply.github.com>
2023-12-19 15:06:08 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const handleAddContact = newContact => {
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const handleDeleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={e => setFilter(e.target.value)} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
+
+export default App;
